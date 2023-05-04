7be5e3539318bd680818c31c86f279649694b09b
Juan Bernardo Benavides Rubio <jbbenavidesr@gmail.com>
2023-05-04 11:43:05 -0500

    :lipstick: Add design tokens configuration to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
     content: ["./src/**/*.{html,js,svelte,ts}"],
     theme: {
-        extend: {},
+        fontFamily: {
+            sansSerif: ["Source Sans Pro", "sans-serif"],
+            mono: ["Source Code Pro", "monospace"],
+            handwriting: ["Kalam", "cursive"],
+        },
+        extend: {
+            colors: {
+                lavenderIndigo: "#8657e1",
+                daisyBush: "#4714a5",
+                goldenFizz: "#feff40",
+                whisper: "#f9f4f9",
+                pastelPurple: "#b2a1bb",
+                purple: "#7209b7",
+                robinEggBlue: "#00e9c0",
+                blueGem: "#3813a0",
+                caribbeanGreen: "#1CC6A0",
+                scarlet: "#f72f45",
+                monsoon: "#777878",
+                silver: "#c0c0c0",
+                gallery: "#efeaf2",
+                fog: "#decaec",
+            },
+        },
     },
     plugins: [],
 };
