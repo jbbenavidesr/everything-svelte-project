4402d04b69176a275c7b3a1ac109967f966e51ec
Juan Bernardo Benavides Rubio <jbbenavidesr@gmail.com>
2023-05-17 10:47:14 -0500

    :lipstick: Add nice active state to links

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,48 @@
+<script>
+    import { page } from "$app/stores";
+</script>
+
+<header class="col-span-3 bg-daisyBush text-center">
+    <div>
+        <a href="/invoices"
+            ><img src="/images/logo.svg" alt="The Dollar Holler" class="mx-auto" /></a
+        >
+    </div>
+
+    <nav>
+        <ul class="list-none text-2xl font-bold">
+            <li>
+                <a href="/invoices" class:active={$page.url.pathname === "/invoices"}>Invoices</a>
+            </li>
+            <li>
+                <a href="/clients" class:active={$page.url.pathname === "/clients"}>Clients</a>
+            </li>
+            <li>
+                <a href="#" class:active={$page.url.pathname === "unknown"}>Settings</a>
+            </li>
+            <li>
+                <a href="#" class:active={$page.url.pathname === "unknown"}>Logout</a>
+            </li>
+        </ul>
+    </nav>
+</header>
+
+<style lang="postcss">
+    nav ul li {
+        @apply mb-6;
+    }
+
+    nav ul li a {
+        @apply font-bold text-white hover:text-goldenFizz;
+    }
+
+    nav ul li a.active {
+        @apply px-8 text-robinEggBlue transition-[padding];
+        background: url(/images/active-nav--left.png) left top no-repeat,
+            url(/images/active-nav--right.png) right top no-repeat;
+    }
+
+    nav ul li a.active:hover {
+        @apply px-9;
+    }
+</style>
--- src/routes/(dashboard)/+layout.svelte
+++ src/routes/(dashboard)/+layout.svelte
@@ -1,39 +1,10 @@
-<div class="grid min-h-screen grid-cols-12 gap-x-16 bg-whisper">
-    <header class="col-span-3 bg-daisyBush text-center">
-        <div>
-            <a href="/invoices"
-                ><img src="/images/logo.svg" alt="The Dollar Holler" class="mx-auto" /></a
-            >
-        </div>
+<script>
+    import Navbar from "$lib/components/Navbar.svelte";
+</script>
 
-        <nav>
-            <ul class="list-none text-2xl font-bold">
-                <li>
-                    <a href="/invoices">Invoices</a>
-                </li>
-                <li>
-                    <a href="/clients">Clients</a>
-                </li>
-                <li>
-                    <a href="#">Settings</a>
-                </li>
-                <li>
-                    <a href="#">Logout</a>
-                </li>
-            </ul>
-        </nav>
-    </header>
+<div class="grid min-h-screen grid-cols-12 gap-x-16 bg-whisper">
+    <Navbar />
     <main class="col-span-8 pt-10">
         <slot />
     </main>
 </div>
-
-<style lang="postcss">
-    nav ul li {
-        @apply mb-6;
-    }
-
-    nav ul li a {
-        @apply font-bold text-white hover:text-goldenFizz;
-    }
-</style>
